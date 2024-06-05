dafea61c14e38b56919fa6597853e8c964e49e5f
BestineOkinda <bestineokinda@gmail.com>
2024-06-05 16:59:54 +0300

    Wrapping up the code challenges solutions

--- code_challenges/challenge_01.R
+++ code_challenges/challenge_01.R
@@ -1,25 +1,89 @@
 #You have to use Base R to solve the questions below;
 
 # 1. If you have 3 apples and you buy 5 more apples. How many apples do you have in total?
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#_  + _
+
 # 2. You have 10 candies and you give away 4 candies. How many candies do you have in total? 
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#_ - _
+
 # 3. Each pen costs 2 US dollars. If you bought 7 pens, how much did you spend in total? 
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+
+#_ * _
+
 # 4. Solve the following equations;
 ## a. `34 + 54`
-## b. `76 - 10`
-## c. `25 * 25`
-## d. if `d=42` and `f=14` evaluate `d/f`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#______
 
-# 5. A rectangle has a length of 8 meters and width of 5 meters. What is the area of the rectangle. Use variables, `length`, `width` and `area` to find the solution. 
+## b. `76 - 10`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#______
+
+## c. `25 * 25`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#______
+
+## d. if `d=42` and `f=14` evaluate `d/f`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#d=_
+#f=_
+#d/f
+
+
+# 5. A rectangle has a length of 8 meters and width of 5 meters. What is the 
+# area of the rectangle. Use variables, `length`, `width` and `area` to find the
+# solution. 
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+# length=_
+# width=_
+
+# area = length * width
+
 # 6. A car travels in a speed of 60km/hr. How far will it travel for 3 hours. 
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#speed = _
+#time = _
+#distance = speed * time
+#distance
+
 # 7. Evaluate;
 ## a. `5 + 3 * 2`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#______________
+
 ## b. `(8 + 2) * (5 - 3)`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#_____________
+
 ## c. `9 + (6 - 2) * 3`
+# FILL THE CODE BELOW HERE
+#______________
+
 ## d. `8 * 3 - 2^2`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#______________
+
 ## e. `(10 + 5) * (2^2) / 4`
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+#_____________
 
-# 8. You have `g` liters of juice. You want to pour the juice equally into the `h` bottles. How much juice will each bottle contain? `g=12`, `h=13`.
+# 8. You have `g` liters of juice. You want to pour the juice equally into the 
+# `h` bottles. How much juice will each bottle contain? let  `g=12`, `h=3`.
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+g=12
+h=13
 
-# 9.You have `b` rupees and spend 8 rupees on a book. How much money do you have left? 
+#___ # find the number of bottles
+
+
+# 9.You have `b` rupees and spend 8 rupees on a book. How much money do you have left? Let `b` be 25
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+# b = _
+# b - _
   
 # 10. Evaluate `m=5 * (4 + 6) - 10` and print out `m` to the console. 
+# UNCOMMENT AND FILL THE CODE BELOW HERE
+# ______________
--- code_challenges/challenge_05.R
+++ code_challenges/challenge_05.R
@@ -1,25 +1,86 @@
 # DPLYR LIBRARY
 
+#Palmer Archipelago(Antarctica) Penguin data - 
+#https://www.kaggle.com/datasets/parulpandey/palmer-archipelago-antarctica-penguin-data
+
+
 # 1. `ToothGrowth` is one of the freely available data sets in base R. Go on and;
 
 ## a. Load the `ToothGrowth` data set. 
+# UNCOMMENT AND FILL THE CODE BELOW
+
+#data("__________")
+
 ## b. Display the first 5 records.
+
+# head(______, _)
 ## c. Use the function `write.table` to write the data set to a local file. 
+# UNCOMMENT AND FILL THE CODE BELOW
+
+#write.table(___, "file path to write data", 
+#            append = FALSE, quote=FALSE, row.names = FALSE)
 
 
 # 2. You will use the Palmer Archipelago(Antarctica) Penguin data set from kaggle to 
 #practice various data manipulation techniques using `dplyr` verbs in R. Ensure You load 
-#the data and necessary packages before working on the following questions below;
+#the data as `penguin_data` and necessary packages before working on the following questions below;
+# UNCOMMENT AND FILL THE CODE BELOW
 
-## a. Using `select()` verb, create a new data set that contains only the columns `species, 
-#island, bill_length_mm`. 
-## b. Filter the Penguin data set to retain only the "Adelie" species that live in 
-#"Torgersen" island. Use `filter()` function.
-## c. Arrange the Penguin data set according to `bill_lenghth_mm` in descending order. 
-#Use `arrange()` verb.
-## d. Using the `mutate()` function, add a new column called `bill_length_cm` derived 
-#from `bill_length_mm` by 10. 
-## e. Group the data set according to `island`. 
+#install.packages("dplyr") # install dplyr library
+#library(_____)
+
+#penguin_data = read.csv("fill with the file path")
+
+
+## a. Using `select()` verb, create a new data set that contains only the columns
+#`Species, Island, Flipper.Length..mm.` and name the dataframe `selected_features`. 
+#Show the first 10 observations of `selected_features`.
+# UNCOMMENT AND FILL THE CODE BELOW
+
+#selected_features = select(_______, # data frame name  
+#                           _______, ______, ____________) # fill with column names
+
+## b. Filter the Penguin data set to retain only the "Adelie Penguin (Pygoscelis adeliae)" species that live in 
+#"Torgersen" island. Use `filter()` function. 
+#Name the resultant data set `Adelie_data` and show its first 7 records
+# UNCOMMENT AND FILL THE CODE BELOW
+
+#Adelie_data = filter(______, # insert the data frame name
+#                     ______=="Adelie Penguin (Pygoscelis adeliae)") # complete the condition
+
+#head(Adelie_data, 7)
+
+
+## c. Arrange the Penguin data set according to `Flipper.Length..mm.` in descending order. 
+#Use `arrange()` verb. Name the resultant data frame, `ordered_penguin_data` and
+#show the first five rows
+# UNCOMMENT AND FILL THE CODE BELOW
+
+#ordered_penguin_data = arrange(_______, # insert the data frame name
+#                               desc(_____________)) # the condition
+
+#head(___________, 5)
+
+## d. Using the `mutate()` function, add a new column called `Flipper.Length..cm.` derived 
+#from `Flipper.Length..mm.` by 10. Name the data set, `modified_penguin_data` and show the first rows
+# UNCOMMENT AND FILL CODE BELOW
+
+#modified_penguin_data = mutate(______________, #insert the data frame name
+#                               Flipper.Length..cm. = ___________/10) # the condition
+
+# head(modified_penguin_data, _)
+
+## e. Group the data set according to `island`. Name the resultant data set, `penguin_data_by_island` 
+#print the data(first 10 rows) to the console.
+# UNCOMMENT AND FILL CODE BELOW
+
+#penguin_data_by_island = group_by(_____________, # insert the data frame name
+#                                  ________) # the column to be grouped by
+
+# head(____________________, 10)
+
+
+
 ## f. For each species and island, calculate the mean `bill_length_mm` and the 
 #observations count. 
 
